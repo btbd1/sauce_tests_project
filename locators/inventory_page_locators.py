c105b1f7aff9475c2f353659f228d4555658b555
btbd1 <annchest1@gmail.com>
2025-02-13 18:04:52 +0400

    add locators for method sorting low-to-high

--- locators/inventory_page_locators.py
+++ locators/inventory_page_locators.py
@@ -6,3 +6,5 @@ class InventoryPageLocators:
     BUTTON_BACKPACK = (By.CSS_SELECTOR, '#add-to-cart-sauce-labs-backpack')
     BUTTON_BIKE_LIGHT = (By.CSS_SELECTOR, '#add-to-cart-sauce-labs-bike-light')
     BUTTON_CART = (By.CSS_SELECTOR,  '.shopping_cart_link')
+    SORT_BUTTON_LOW_HIGH = (By.CSS_SELECTOR, '.product_sort_container [value="lohi"]')
+    ITEM_PRICE = (By.CSS_SELECTOR, '.inventory_item_price')
